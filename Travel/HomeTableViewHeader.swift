40040ffba699efc881069b402e21324a79fff3f0
Hector Fortuna <hectorsfortuna@gmail.com>
2023-06-17 19:28:35 -0700

    Config Header

---
+++ Travel/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  Travel
+//
+//  Created by Hector on 17/06/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
